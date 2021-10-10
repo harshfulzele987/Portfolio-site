7e7ee30685d26ee61ba7613e8e03bd2eabf213d8
harsh fulzel <harshfulzele987@gmail.com>
2021-10-11 00:33:02 +0530

    push

---
+++ js/app.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows:false,
+        dots:true,
+        appendDots:'.slider-dots',
+        dotsClass:'dots'
+
+    });
+});
+
+
+let hamberger =document.querySelector('.hamberger');
+let times=document.querySelector('.times');
+let mobileNav=document.querySelector('.mobile-nav');
+
+function showDiv() {
+    div = document.getElementById('#con-button');
+    div.style.display = "block";
+}
+hamberger.addEventListener('click' ,function(){
+    mobileNav.classList.add('open')
+
+});
+
+
+times.addEventListener('click' ,function(){
+    mobileNav.classList.remove('open')
+
+
+});
